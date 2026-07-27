95bcb6a23e6ebbbc9f38ed8b1bd90e3b0913cf71
harshit saini <harshitsainidkr@gmail.com>
2026-07-27 16:22:11 +0530

    Updated New changes

--- src/main/java/com/endfielders/erl/repository/CarrierRepository.java
+++ src/main/java/com/endfielders/erl/repository/CarrierRepository.java
@@ -2,11 +2,9 @@ package com.endfielders.erl.repository;
 
 import com.endfielders.erl.model.Carrier;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository
 public interface CarrierRepository extends JpaRepository<Carrier, Long> {
 
     List<Carrier> findByActiveStatusTrue();
